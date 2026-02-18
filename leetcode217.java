31a5fe07d21dfb179f606a6b784336d423334e4b
23f3002181 <23f3002181@ds.study.iitm.ac.in>
2026-02-19 00:15:25 +0530

    Implement containsDuplicate using HashSet

---
+++ leetcode217.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+        for(int num:nums){
+            if(seen.contains(num)){
+                return true;
+            }
+            seen.add(num);
+        }
+        return false;
+        // for (int i = 0; i < nums.length; i++) {
+        //     for (int j = i + 1; j < nums.length; j++) {
+        //         if (nums[i] == nums[j]) {
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
+
+        // Arrays.sort(nums);
+        // for(int i = 1; i< nums.length; i++){
+        //     if(nums[i]==nums[i-1]){
+        //         return true;
+        //     }
+        // }
+        // return false;
+        
+        // return Arrays.stream(nums)
+        //              .distinct()
+        //              .count() < nums.length;
+    }
+}

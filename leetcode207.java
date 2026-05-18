4681b7389c12f155e9996b2da4757161366c5e08
23f3002181 <23f3002181@ds.study.iitm.ac.in>
2026-05-18 21:37:11 +0530

    Implement course scheduling using Kahn's Algorithm

---
+++ leetcode207.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> graph = new ArrayList<>() ;
+        for(int i = 0 ; i < numCourses ; i++){
+            graph.add(new ArrayList<>()) ;
+        }
+        int[] indegree = new int[numCourses] ;
+        for(int[] pre : prerequisites){
+            int a = pre[0] ;
+            int b = pre[1] ;
+            graph.get(b).add(a) ;
+            indegree[a]++ ;
+        }
+        Queue<Integer> q = new LinkedList<>() ;
+        for(int i = 0 ; i < numCourses ; i++){
+            if(indegree[i] == 0){
+                q.offer(i) ;
+            } 
+        }
+        int count = 0 ;
+        while(!q.isEmpty()){
+            int node = q.poll() ;
+            count++ ;
+            for(int neighbour : graph.get(node)){
+                indegree[neighbour]-- ;
+                if(indegree[neighbour] == 0){
+                    q.offer(neighbour) ;
+                }
+            }
+        }
+        return count == numCourses ;
+    }
+}

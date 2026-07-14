96f528d72e3b3de7216a96004dc444cdd41ed7b3
23f3002181 <23f3002181@ds.study.iitm.ac.in>
2026-07-14 21:40:28 +0530

    Find All Duplicates in an Array

---
+++ leetcode442.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        Map<Integer,Integer> map = new HashMap<>() ;
+        for(int num : nums){
+            map.put(num, map.getOrDefault(num,0)+1) ;
+        }
+        List<Integer> ans = new ArrayList<>() ;
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+            if(entry.getValue() == 2){
+                ans.add(entry.getKey()) ;
+            }
+        }
+        return ans ;
+    }
+}

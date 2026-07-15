7bcff811812c8b79e53fbb046474b077fbfab3ce
Gautam Bhatt <23f3002181@ds.study.iitm.ac.in>
2026-07-15 17:31:24 +0530

    Sort Elements of an Array by Frequency
    Implement frequency sort for an array of integers.

---
+++ leetcode1636.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] frequencySort(int[] nums) {
+        Map<Integer,Integer> freq = new HashMap<>() ;
+        for(int num : nums){
+            freq.put(num, freq.getOrDefault(num,0) + 1) ;
+        }
+        List<Integer> list = new ArrayList<>() ;
+        for(int num : nums){
+            list.add(num) ;
+        }
+        Collections.sort(list, (a,b) -> {
+            if(!freq.get(a).equals(freq.get(b))) return freq.get(a) - freq.get(b) ;
+            return b - a ;
+        }) ;
+        for(int i = 0 ; i < nums.length ; i++){
+            nums[i] = list.get(i) ;
+        }
+        return nums ;
+    }
+}
